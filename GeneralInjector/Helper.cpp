87b32b7c2c09b78ebab520d5f3d200d9fb8329bc
铮 杨 <mouka0905@outlook.com>
2018-11-30 18:05:43 +0800

    add sorting process list ; add retriving selected process id

--- GeneralInjector/Helper.cpp
+++ GeneralInjector/Helper.cpp
@@ -4,64 +4,6 @@
 #include <strsafe.h>
 #include "Helper.h"
 
-static BOOL SetPrivilege(
-     HANDLE hToken,          // token handle
-     LPCTSTR Privilege,      // Privilege to enable/disable
-     BOOL bEnablePrivilege   // TRUE to enable.  FALSE to disable
-)
-{
-     TOKEN_PRIVILEGES tp;
-     LUID luid;
-     TOKEN_PRIVILEGES tpPrevious;
-     DWORD cbPrevious = sizeof( TOKEN_PRIVILEGES );
-
-     if ( !LookupPrivilegeValue( NULL, Privilege, &luid ) ) return FALSE;
-
-     // 
-     // first pass.  get current privilege setting
-     // 
-     tp.PrivilegeCount = 1;
-     tp.Privileges[0].Luid = luid;
-     tp.Privileges[0].Attributes = 0;
-
-     AdjustTokenPrivileges(
-          hToken,
-          FALSE,
-          &tp,
-          sizeof( TOKEN_PRIVILEGES ),
-          &tpPrevious,
-          &cbPrevious
-     );
-
-     if ( GetLastError() != ERROR_SUCCESS ) return FALSE;
-
-     // 
-     // second pass.  set privilege based on previous setting
-     // 
-     tpPrevious.PrivilegeCount = 1;
-     tpPrevious.Privileges[0].Luid = luid;
-
-     if ( bEnablePrivilege ) {
-          tpPrevious.Privileges[0].Attributes |= ( SE_PRIVILEGE_ENABLED );
-     }
-     else {
-          tpPrevious.Privileges[0].Attributes ^= ( SE_PRIVILEGE_ENABLED &
-               tpPrevious.Privileges[0].Attributes );
-     }
-
-     AdjustTokenPrivileges(
-          hToken,
-          FALSE,
-          &tpPrevious,
-          cbPrevious,
-          NULL,
-          NULL
-     );
-
-     if ( GetLastError() != ERROR_SUCCESS ) return FALSE;
-
-     return TRUE;
-}
 
 
 static BOOLEAN EnableDebugPrivilege()
