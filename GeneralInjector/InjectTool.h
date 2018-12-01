c2716753598454da168138ad302a1f75eedaec20
mouka <mouka0905@gmail.com>
2018-12-02 01:28:32 +0800

    refactor code...

--- GeneralInjector/InjectTool.h
+++ GeneralInjector/InjectTool.h
@@ -16,18 +16,30 @@ class InjectTool
 {
 	DWORD m_InjectType;
 	DWORD m_TargetPid;
+	DWORD m_TargetTid;
 	CString m_TargetDll;
+	HWND m_TargetWindow;
 
 public:
 	// Constructor
-	InjectTool() : m_InjectType(INJECT_CREATE_REMOTE_THREAD), m_TargetPid(0), m_TargetDll("") {}
+	InjectTool() : 
+		m_InjectType(INJECT_CREATE_REMOTE_THREAD), 
+		m_TargetPid(0), 
+		m_TargetDll(""),
+		m_TargetTid(0),
+		m_TargetWindow(NULL){}
 
 	CString GetTargetDll() { return m_TargetDll; }
 	DWORD GetTargetProcess() { return m_TargetPid; }
 	DWORD GetInjectType() { return m_InjectType; }
+	DWORD GetTargetThread() { return m_TargetTid; }
+	HWND GetTargetWindow() { return m_TargetWindow; }
+
 	void SetInjectType(DWORD InjectType) { m_InjectType = InjectType; }
 	void SetTargetProcess(DWORD Pid) { m_TargetPid = Pid; }
 	void SetTargetDll(CString TargetDll) { m_TargetDll = TargetDll; }
+	void SetTargetThread(DWORD Tid) { m_TargetTid = Tid; }
+	void SetTargetWindow(HWND hWnd) { m_TargetWindow = hWnd; }
 
 	BOOLEAN Inject();
 	BOOLEAN InjectCreateRemoteThread();
