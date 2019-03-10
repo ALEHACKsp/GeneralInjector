822f7cc6e0136c423d0faba0331b9c617b62d75a
mouka <mouka0905@gmail.com>
2019-03-10 13:23:03 +0800

    ...

--- GeneralInjector/PEHelper.cpp
+++ GeneralInjector/PEHelper.cpp
@@ -17,14 +17,14 @@ BOOLEAN PEHelper::Analyze( BOOLEAN Force ) {
 		PIMAGE_NT_HEADERS32 NtHeader32 = (PIMAGE_NT_HEADERS32)NtHeader;
 		Is64Mod = NtHeader32->OptionalHeader.Magic == IMAGE_NT_OPTIONAL_HDR64_MAGIC;
 		if ( Is64Mod ) {
-			SecHeader = (PIMAGE_SECTION_HEADER)( (ULONG_PTR)NtHeader64 + NtHeader64->OptionalHeader.SizeOfHeaders );
+			SecHeader = (PIMAGE_SECTION_HEADER)( (ULONG_PTR)NtHeader64 + sizeof(IMAGE_NT_HEADERS64) );
 			ImageSize = NtHeader64->OptionalHeader.SizeOfImage;
 			DefaultBase = NtHeader64->OptionalHeader.ImageBase;
 			EntryPoint = NtHeader64->OptionalHeader.AddressOfEntryPoint != 0 ?
 				ImageBase + NtHeader64->OptionalHeader.AddressOfEntryPoint : 0;
 		}
 		else {
-			SecHeader = (PIMAGE_SECTION_HEADER)( (ULONG_PTR)NtHeader32 + NtHeader32->OptionalHeader.SizeOfHeaders );
+			SecHeader = (PIMAGE_SECTION_HEADER)( (ULONG_PTR)NtHeader32 + sizeof(IMAGE_NT_HEADERS32) );
 			ImageSize = NtHeader32->OptionalHeader.SizeOfImage;
 			DefaultBase = NtHeader32->OptionalHeader.ImageBase;
 			EntryPoint = ImageBase + NtHeader32->OptionalHeader.AddressOfEntryPoint;
